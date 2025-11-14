eec285fcfddf350654e3958159dd87dbc884bad8
sunnapu_swetha <sunnapuswethareddy@gmail.com>
2025-11-14 21:09:31 +0530

    Added evenOdd function

---
+++ evenOdd.js
@@ -0,0 +1,10 @@
+function evenOdd(num) {
+    if (num % 2 === 0) {
+        return true; // even
+    } else {
+        return false; // odd
+    }
+}
+
+console.log(evenOdd(10)); // true
+console.log(evenOdd(7));  // false
